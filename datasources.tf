63e7027cbeb61cc708880f9d2e205953868e8ad2
Dmytro <dima.shutko5@gmail.com>
2026-05-31 22:38:27 +0200

    test

--- datasources.tf
+++ datasources.tf
@@ -22,3 +22,17 @@ data "azurerm_virtual_machine" "name" {
   name                = "${var.prefix}-vm"
   resource_group_name = data.azurerm_resource_group.example.name
 }
+
+resource "azurerm_virtual_machine_extension" "example" {
+  name                 = "hostname"
+  virtual_machine_id   = data.azurerm_virtual_machine.name.id
+  publisher            = "Microsoft.Azure.Extensions"
+  type                 = "CustomScript"
+  type_handler_version = "2.0"
+
+  settings = <<SETTINGS
+ {
+  "commandToExecute": "sudo apt update -y && sudo apt upgrade -y && sudo apt install -y nginx && echo Terraform >> /usr/share/nginx/html/index.html"
+ }
+SETTINGS
+}
